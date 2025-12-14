f5ed1c15a256d1c1e8da44d05245c518299a3ecf
Puskar Thapa Magar <puskarmagar0215@gmail.com>
2025-12-14 22:23:41 +0545

    Added all needed sources

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import About from "./Components/About";
@@ -6,9 +6,17 @@ import About from "./Components/About";
 import Contact from "./Components/Contact";
 import Footer from "./Components/Footer";
 import Projects from "./Components/Project";
-
+import favicon from "./assets/portfolio-icon.png";
 
 const App = () => {
+  useEffect(() => {
+    document.title = "Puskar Thapa Magar | Portfolio";
+    const link = document.querySelector("link[rel~='icon']") || document.createElement("link");
+    link.rel = "icon";
+    link.href = favicon;
+    document.head.appendChild(link);
+  }, []);
+
   return (
     <div className="bg-[#020b0f] min-h-screen text-white">
       <Navbar />
